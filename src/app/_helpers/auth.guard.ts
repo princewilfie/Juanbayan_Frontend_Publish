748927ea17b6f6d1125b16607731a71bcac1c107
temporary1581@gmail.com <temporary1581@gmail.com>
2024-08-12 20:40:32 +0800

    Mo run nani siya pero tarongon pa ang mga UI

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AccountService } from '@app/_services';
 
@@ -8,20 +8,24 @@ export class AuthGuard {
     constructor(
         private router: Router,
         private accountService: AccountService
-    ) {}
+    ) { }
 
-    canActive(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const account = this.accountService.accountValue;
         if (account) {
+            // check if route is restricted by role
             if (route.data.roles && !route.data.roles.includes(account.role)) {
+                // role not authorized so redirect to home page
                 this.router.navigate(['/']);
                 return false;
             }
 
+            // authorized so return true
             return true;
         }
 
-        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+        // not logged in so redirect to login page with the return url 
+        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
 }
