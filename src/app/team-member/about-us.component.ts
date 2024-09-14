b1b267498736389ee653467175b0b2726f533808
temporary1581@gmail.com <temporary1581@gmail.com>
2024-09-14 16:11:31 +0800

    Admin Dashboard

---
+++ src/app/team-member/about-us.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-member',
+  templateUrl: './team-member.component.html'
+})
+export class TeamMemberComponent {
+  // Add logic if needed later.
+}
